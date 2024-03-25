bcc10044cec5c3ec573db3c879f329aba36ba3e0
Rahmat Faizan <99715869+RahmatFaizan@users.noreply.github.com>
2024-03-25 07:20:09 -0700

    Add files via upload
    Monday, 25 March 2024

--- routes/sendAppointment.js
+++ routes/sendAppointment.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {makeApiCall} = require("../global/globalFns");
 
-router.get("/appointment/send", async (req, res) => {
+router.post("/appointment/send", async (req, res) => {
     let error;
     try {
         console.log("body", req.body);
     } catch (err) {
         error = err;
     }
-    res.status(200).json({msg: "Webhook received successfully", error});
+    res.status(200).json({msg: "Webhook received successfully", error, body: req.body});
 })
 
 module.exports = router;
