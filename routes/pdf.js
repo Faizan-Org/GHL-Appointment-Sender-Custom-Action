cc2d84a0c2cd867c3a8975ca5302fa175618db55
Rahmat Faizan <99715869+RahmatFaizan@users.noreply.github.com>
2024-03-26 12:21:25 -0700

    Add files via upload

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("contact/pdf", async (req, res) => {
+router.post("/contact/pdf", async (req, res) => {
     console.log("Contact PDF", req.body)
     res.status(200).send("Successfully created...");
 })
