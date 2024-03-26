9b9ce5561561ade04a09bb09c9dbf9fba36a27e8
Rahmat Faizan <99715869+RahmatFaizan@users.noreply.github.com>
2024-03-26 12:19:21 -0700

    Add files via upload
    12:19 am Wednesday, 27 March 2024

---
+++ routes/pdf.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("contact/pdf", async (req, res) => {
+    console.log("Contact PDF", req.body)
+    res.status(200).send("Successfully created...");
+})
+
+module.exports = router;
