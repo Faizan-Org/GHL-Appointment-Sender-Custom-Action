e44bb87a327dc6dc40caac1e39823ef06f915f44
Rahmat Faizan <99715869+RahmatFaizan@users.noreply.github.com>
2024-04-22 06:58:23 -0700

    6:58 pm Monday, 22 April 2024

--- index.js
+++ index.js
@@ -5,23 +5,48 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-    require("dotenv").config();
-}
+const {WebSocketServer} = require("ws");
+const http = require('http');
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
 
 index.use(express.json());
 index.use(bodyParser.urlencoded({extended: true}));
 index.use(bodyParser.json());
-index.use(morgan("dev"));
+index.use(morgan('tiny'));
 index.use(cors());
 index.use(helmet());
 
+const server = http.createServer(index);
+const wss = new WebSocketServer({server});
+const clients = new Set();
+
+wss.on("connection", (socket, req) => {
+    console.log('A new client connected:', req.socket.remoteAddress);
+    clients.add(socket);
+
+    wss.on('error', console.error);
+
+    socket.on("close", () => {
+        console.log("Client disconnected");
+        clients.delete(socket);
+    });
+
+});
+
+index.use((req, res, next) => {
+    req.wss = wss;
+    req.clients = clients;
+    next();
+});
+
 index.use(Routes);
+
 index.get("*", (_, res) => {
     res.status(200).send("Running... OK!");
 })
 
-index.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
